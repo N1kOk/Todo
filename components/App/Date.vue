f53b97188728ae2de1b0bcc34f681a143bcca8d1
N1kOk <n1k_ok@bk.ru>
2023-03-01 02:30:16 +0500

    feat: add AppDate component

---
+++ components/App/Date.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="flex space-x-1 text-[10px]">
+		<div class="text-gray-300">
+			<div>Дата создания:</div>
+			<div>Дата окончания:</div>
+		</div>
+		<div class="text-blue">
+			<div>{{ start }}</div>
+			<div>{{ end }}</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { Todo } from '~/types'
+
+interface AppDateProps {
+	start: Todo['startDate']
+	end: Todo['endDate']
+}
+
+const { start, end } = defineProps<AppDateProps>()
+</script>
