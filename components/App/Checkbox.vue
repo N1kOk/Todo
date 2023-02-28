d523a5a9c1350144845f94c598b0d44b66b4a213
N1kOk <n1k_ok@bk.ru>
2023-02-28 18:38:21 +0500

    feat: add Checkbox component

---
+++ components/App/Checkbox.vue
@@ -0,0 +1,38 @@
+<template>
+	<div
+		class="checkbox"
+		:class="classes"
+		@click="isChecked = !isChecked"
+	>
+		<IconCheck class="relative top-[1px]" v-show="isChecked"/>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from '#imports'
+
+const isChecked = ref(false)
+
+const classes = computed(() => ({
+	'checkbox--checked': isChecked.value,
+	'checkbox--unchecked': !isChecked.value,
+}))
+</script>
+
+<style scoped>
+.checkbox {
+	@apply flex justify-center items-center w-4 h-4 border-2 cursor-pointer transition-colors rounded-full;
+}
+
+.checkbox--checked {
+	@apply
+	bg-purple-dark border-purple-dark
+	hover:bg-purple hover:border-purple;
+}
+
+.checkbox--unchecked {
+	@apply
+	border-blue
+	hover:bg-blue-dark/25 hover:border-blue-dark;
+}
+</style>
