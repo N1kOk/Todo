1227da2679bd0453661402030e5e6ddefbfc5459
N1kOk <n1k_ok@bk.ru>
2023-02-28 18:37:31 +0500

    feat: add icons

---
+++ components/Icon/Check.vue
@@ -0,0 +1,7 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="10" height="7" viewBox="0 0 10 7" fill="none">
+		<path
+			d="M8.43071 0.342154L4.09877 4.67409L1.6163 2.19162L0.780396 3.02753L4.09877 6.3459L9.26661 1.17806L8.43071 0.342154Z"
+			fill="#F2F2F2"/>
+	</svg>
+</template>
