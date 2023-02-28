f09372f6ba17b2514b35dee56c83e009deec5372
N1kOk <n1k_ok@bk.ru>
2023-02-28 18:38:34 +0500

    feat: add TodoItem component

---
+++ components/Todo/Item.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="p-4 pb-8 bg-gray-500 rounded">
+		<div class="flex items-start space-x-4">
+			<AppCheckbox/>
+			<div class="flex flex-col flex-1 space-y-4">
+				<div class="flex justify-between items-center">
+					<h2 class="text-xl font-bold">Title</h2>
+					<div class="flex items-center space-x-4">
+						<div class="flex space-x-1 text-[10px]">
+							<div class="text-gray-300">
+								<div>Дата создания:</div>
+								<div>Дата окончания:</div>
+							</div>
+							<div class="text-blue">
+								<div>27.02.2023</div>
+								<div>27.03.2023</div>
+							</div>
+						</div>
+						<IconTrash class="hover:text-red-400"/>
+						<IconEdit/>
+					</div>
+				</div>
+
+				<p class="text-sm">Lorem ipsum dolor sit amet</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
